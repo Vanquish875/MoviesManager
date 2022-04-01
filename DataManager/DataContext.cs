91a55e5207972dc4adbc60a7985e2ce336d146a1
Vanquish875 <Vanquish87523@gmail.com>
2022-04-01 11:25:41 -0500

    Add get and post movies

---
+++ DataManager/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using MoviesManager.Models;
+
+namespace MoviesManager.DataManager
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+global using Microsoft.EntityFrameworkCore;
+global using MoviesManager.DataManager;
+using MoviesManager.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("moviedb"));
 
 var app = builder.Build();
 
@@ -16,5 +21,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/movies", async (DataContext context) => await context.Movies.ToListAsync());
+
+app.MapPost("/movies", async (DataContext context, Movie movie) =>
+{
+    context.Movies.Add(movie);
+    await context.SaveChangesAsync();
+    return Results.Ok(await context.Movies.ToListAsync());
+});
 
 app.Run();
